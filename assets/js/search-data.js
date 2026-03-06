dd17b4f5055f84cf21e87ba7b802fc9aa18b41f7
TiandaSun <94608923+TiandaSun@users.noreply.github.com>
2026-03-06 14:21:21 +0000

    Deploying to gh-pages from @ TiandaSun/TiandaSun.github.io@6ab8a075a7e58e2e6312dbda4e960daa438d1444 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,96 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-presented-hybrid-qa-model-at-dao-xai-2024-workshop-at-ecai-santiago-de-compostela-spain",
+          title: 'Presented hybrid QA model at DAO-XAI 2024 workshop at ECAI, Santiago de Compostela,...',
+          description: "",
+          section: "News",},{id: "news-paper-on-kinshipqa-benchmark-submitted-to-acl-2026-and-available-on-arxiv",
+          title: 'Paper on KinshipQA benchmark submitted to ACL 2026 and available on arXiv.',
+          description: "",
+          section: "News",},{id: "news-kgeir-paper-accepted-at-ranlp-2025-workshop-r2lm-in-varna-bulgaria",
+          title: 'KGEIR paper accepted at RANLP 2025 Workshop (R2LM) in Varna, Bulgaria.',
+          description: "",
+          section: "News",},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/Tianda_Sun_CV.pdf", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%74%69%61%6E%64%61.%73%75%6E@%79%6F%72%6B.%61%63.%75%6B", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/TiandaSun", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0003-1998-5228", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
